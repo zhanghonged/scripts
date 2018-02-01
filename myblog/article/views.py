ea1a51c33c47cb4c139fc263307068323252cee2
zhanghonged <zhanghonged@126.com>
2018-02-01 15:25:31 +0800

    修改分页，扩展Paginator实现显示固定数据的页码范围

--- myblog/article/views.py
+++ myblog/article/views.py
@@ -5,31 +5,45 @@ from models import Article
 from django.core.paginator import Paginator ,EmptyPage ,PageNotAnInteger
 # Create your views here.
 
+# 扩展自定义表单分页
+class CustomPaginator(Paginator):
+    def __init__(self ,current_page, per_pager_num, *args, **kwargs):
+        # 当前页
+        self.current_page = int(current_page)
+        # 页码最大显示范围
+        self.per_pager_num = per_pager_num
+        # 继承父类Paginator的其他属性方法
+        Paginator.__init__(self, *args, **kwargs)
+    def pager_num_range(self):
+        if self.num_pages < self.per_pager_num:
+            return range(1, self.num_pages + 1)
+        part = int(self.per_pager_num / 2)
+        if self.current_page <= part:
+            return range(1, self.per_pager_num + 1)
+        if (self.current_page + part) > self.num_pages:
+            return range(self.num_pages - self.per_pager_num + 1, self.num_pages + 1)
+        else:
+            return range(self.current_page - part, self.current_page + part + 1)
 
 def Article_list(request):
     cus_list = Article.objects.all()
-
-
-    # 实例化结果集，定义每页显示4条数据
-    paginator = Paginator(cus_list,4)
-    # 接收网页中的page值
-    page = request.GET.get('page')
-    # 获取当前页对象的元素列表
-    if page:
-        article_list = paginator.page(page).object_list
-    else:
-        article_list = paginator.page(1).object_list
-    # 获取page页的分页对象
     try:
-        customer = paginator.page(page)
-    # 页码不会一个整数异常，赋值为第一页
+        current_page = int(request.GET.get('page'))
+    except:
+        current_page = 1
+    # 实例化分页对象,每页6条数据,页面显示范围为6
+    paginator = CustomPaginator(current_page,5,cus_list,6)
+    try:
+        # 取对象的当前页分页对象
+        posts = paginator.page(current_page)
+    # current_page非数字时取第一页
     except PageNotAnInteger:
-        customer = paginator.page(1)
-    # 空页码异常，赋值为最后一页
+        posts = paginator.page(1)
+    # current_page空时取最后一页
     except EmptyPage:
-        customer = paginator.page(paginator.num_pages)
-    return render(request, 'article_list.html', {'cus_list':customer,'article_list':article_list})
+        posts = paginator.page(paginator.num_pages)
 
+    return render(request,'article_list.html',{'posts':posts})
 
 def Article_content(request,article_id):
     current_id = int(article_id)
